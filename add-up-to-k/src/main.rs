65f2e835314c0098183a4155add0a97875378e68
eathren <nolan.braman@gmail.com>
2020-10-01 12:43:47 -0400

    added brute force method

--- add-up-to-k/src/main.rs
+++ add-up-to-k/src/main.rs
@@ -1,28 +1,42 @@
-fn main() {
-    let array:[i32; 4]= [10, 15, 3, 7];
-    let k = 17;
-    let mut solved = false;
-   
-    // for pair in array.chunks(2) {
-    //     println!("({}, {})", pair[0], pair[2]);
-    // }
 
-    for number in array.iter(){
-       if number % 2 == 0{
-           println!("Even")
-       }
-       else {
-           println!("Odd")
-       }
+// This problem was recently asked by Google.
+
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+// Bonus: Can you do this in one pass?
+
+fn main() {
+    // see note 1
+    let numbers= vec! [10, 15, 3, 7];
+    let k = 17;
+   
+   // Approach 1: Brute force via iterating through each element, then loop through each element again to 
+   // see if sums add up to k
+
+    for i in 0..numbers.len(){
+        for j in (i+1)..numbers.len(){
+            if numbers[j] == k - numbers[i]{
+                return println!("{} & {} add up to {}", numbers[i], numbers[j],k);
+            }
+        }
     }
 
-    // for (i,x) in array.iter(){
-    //     println!("Item {} = {}", i, x);
-    // }
+    
 
+  
+
+ 
 
 
 }
+
+// notes
+
+// 1
+// Vector is a 'growable' array. I used it here for easy iteration and accessibility in the iteration
+// docs: https://www.cs.brandeis.edu/~cs146a/rust/doc-02-21-2015/book/arrays-vectors-and-slices.html
 
 
   
