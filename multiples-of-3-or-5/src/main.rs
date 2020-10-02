c8f01188571a341374fccaf3a19cf825159ea2f9
eathren <nolan.braman@gmail.com>
2020-10-02 12:07:38 -0400

    added more verbose answer version

--- multiples-of-3-or-5/src/main.rs
+++ multiples-of-3-or-5/src/main.rs
@@ -16,9 +16,22 @@ fn main() {
 }
 
 // to break this one down, first we iterate over the range, and filter out
-// only the values that are divisible by 3 or 5, we then sum that number
+// only the values that are divisible by 3 or 5, we then sum that number.
+// note that there are simpler, more verbose ways to write this. See below.
 fn solution(num: i32) -> i32 {
     (1..num).filter(|&n| n % 3 == 0 || n % 5 == 0).sum()
 }
 
 
+
+// for ease of reading, you could try this method instead. 
+
+// fn solution(num: i32) -> i32 {
+//     let mut sum = 0;
+//     for count in 1..num {
+//         if count % 3 == 0 || count % 5 == 0 {
+//             sum = sum + count;
+//         } 
+//     }
+//     sum
+//   }
